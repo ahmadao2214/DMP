c7ddf1a7495b0435e8bc18d3b9450fa30087cda7
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2016-01-12 01:16:16 -0600

    Split into String Array from ArrayList of movies and removed first line in txt file (will cause problems pending on format of input file)

--- TheatreManagement.java
+++ TheatreManagement.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class TheatreManagement {
-	
-	static BufferedReader bReader = null;
-	static String bLine = "";
-	
 	public static void main(String[] args) {
 		try{
 			readFile(args[0]);
@@ -16,13 +13,33 @@ public class TheatreManagement {
 		catch(Exception e){
 			System.out.println(e);
 		}
-		
 	}
+	
+	static BufferedReader bReader = null;
+	static String bLine = "";
+	static ArrayList<String> bLineArray = new ArrayList<String>();
+	static String singleMovie[] = new String[200];
+	static DateFormat df = new SimpleDateFormat("H:m");
 	
 	public static void readFile(String fileName) throws IOException {
 		bReader = new BufferedReader(new FileReader(fileName));
 		while ((bLine = bReader.readLine()) != null) {
-			System.out.println(bLine);
-		}		
+			//System.out.println(bLine);
+			bLineArray.add(bLine);
+		}	
+		getSingleMovie(bLineArray);
+	}
+	
+	public static void getSingleMovie(ArrayList<String> movies){
+		for(String movie : movies){
+			singleMovie = movie.split(",");
+			for(int i = 0; i < singleMovie.length; i++){
+				System.out.print(singleMovie[i] + " ");
+			}
+		}
+	}
+	
+	public static void createShowTimes(String[] singleMovie){
+		
 	}
 }
