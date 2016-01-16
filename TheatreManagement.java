3719a1c41a84e92dc1847e1796709aebafb4104c
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2016-01-16 01:42:07 -0600

    Testing Date to String conversion using SimpleDateFormat with convertIntoMinutes method all in main. Need to refactor and switch to 24 hour SimpleDateFormat Pattern.

--- TheatreManagement.java
+++ TheatreManagement.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,10 +11,22 @@ public class TheatreManagement {
 		try {
 			Date date = new Date();
 			System.out.println(date.toString());
-			System.out.println(strDays[now.get(Calendar.DAY_OF_WEEK) - 1] + " " +
-					(now.get(Calendar.MONTH) + 1) + "/" + 
-					now.get(Calendar.DATE) + "/" + 
-					now.get(Calendar.YEAR) + " ");
+			System.out.println(strDays[now.get(Calendar.DAY_OF_WEEK) - 1] + " " + (now.get(Calendar.MONTH) + 1) + "/"
+					+ now.get(Calendar.DATE) + "/" + now.get(Calendar.YEAR) + " ");
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
+			Date tmp = sdf.parse(weekdayOpen);
+			String wdo = sdf.format(tmp).toString();
+			StringBuilder sb = new StringBuilder(wdo);
+			sb.deleteCharAt(sb.indexOf("M"));
+			for(int i = 0; i < sb.length(); i++){
+				if(sb.charAt(i) == 'A' || sb.charAt(i) == 'P'){
+					sb.deleteCharAt(i);
+				}
+			}
+			wdo = sb.toString();
+			System.out.println(convertIntoMinutes(wdo));
+
 			readFile(args[0]);
 		} catch (Exception e) {
 			System.out.println(e);
@@ -24,15 +37,18 @@ public class TheatreManagement {
 	static String bLine = "";
 	static ArrayList<String> bLineArray = new ArrayList<String>();
 	static String movieInfo[] = new String[200];
-	
+
 	static Calendar now = Calendar.getInstance();
-	static String[] strDays = new String[] { 
-			"Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"
-	};
+	static String[] strDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday",
+			"Saturday" };
 	static int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);
 	static boolean weekend = false;
-	
 
+	static String weekdayOpen = "12:00 PM";
+	static String weekdayClose = "11:00 PM";
+	static String weekendOpen = "11:30 AM";
+	static String weekendClose = "11:30 PM";
+ 
 	public static void readFile(String fileName) throws IOException {
 		bReader = new BufferedReader(new FileReader(fileName));
 		while ((bLine = bReader.readLine()) != null) {
@@ -56,26 +72,33 @@ public class TheatreManagement {
 			singleMovieTitleSplitArray = singleMovie[i].split(","); 
 			formattedTitle = singleMovieTitleSplitArray[0] + " - Rated " + singleMovieTitleSplitArray[2] + ", " + singleMovieTitleSplitArray[3];
 			movieDuration = convertIntoMinutes(singleMovieTitleSplitArray[3]);
-			//System.out.println(formattedTitle + " = " + movieDuration + " minutes");
+			System.out.println(formattedTitle);
 			if(!isWeekend(dayOfWeek)){
-				
+					
 				}
 			else{
-			
+				
 			}
 		}
 	}
-	
+
 	public static int convertIntoMinutes(String md){
 		int minutes=0;
 		md = md.trim();
+		StringBuilder sb = new StringBuilder(md);
+		for(int i = 0; i < sb.length(); i++){
+			if(sb.charAt(i) == 'A' || sb.charAt(i) == 'P' || sb.charAt(i) == 'M' ){
+				sb.deleteCharAt(i);
+			}
+			md = sb.toString();
+		}
 	    String[] arr= md.split(":");
 	    if(arr.length==2){
 	        minutes=Integer.parseInt(arr[0])*60+Integer.parseInt(arr[1]);
 	    }
 		return minutes;
 	}
-	
+
 	public static boolean isWeekend(int day){
 		if (day == 1 || day == 6 || day == 7)
 			weekend = true;
