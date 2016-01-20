e3624c1f7fab3057363d198d8d5dbdb9631914cd
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2016-01-19 20:53:46 -0600

    Used teststrings to determine position of output

--- TheatreManagement.java
+++ TheatreManagement.java
@@ -10,17 +10,17 @@ import java.util.Date;
 public class TheatreManagement {
 	public static void main(String[] args) {
 		try {
-			Date date = new Date();
-			System.out.println(date.toString());
-			System.out.println(strDays[now.get(Calendar.DAY_OF_WEEK) - 1] + " " + (now.get(Calendar.MONTH) + 1) + "/"
-					+ now.get(Calendar.DATE) + "/" + now.get(Calendar.YEAR) + " ");
-			
-			System.out.println(convertIntoMinutes(convertIntoDate(weekdayClose)));
+			System.out.println(printCurrentDate());
+			//System.out.println(convertIntoMinutes(dateString(weekdayClose)));
+			//System.out.println(dateString(weekdayOpen) + "  - " + dateString(weekdayClose));
 			readFile(args[0]);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+	
+	
+	
 
 	static BufferedReader bReader = null;
 	static String bLine = "";
@@ -30,22 +30,23 @@ public class TheatreManagement {
 	static Calendar now = Calendar.getInstance();
 	static String[] strDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday" };
 	static int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);
-	static boolean weekend = false;
-	
 	static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+	static Date day = new Date();
 
 	static String weekdayOpen = "11:00";
 	static String weekdayClose = "23:00";
 	static String weekendOpen = "11:30";
 	static String weekendClose = "23:30";
+	
+	
+	
  
 	public static void readFile(String fileName) throws IOException {
 		bReader = new BufferedReader(new FileReader(fileName));
 		while ((bLine = bReader.readLine()) != null) {
 			bLineArray.add(bLine);
 		}
-		bLineArray.remove(0);
-		for (int i = 0; i < bLineArray.size(); i++)
+		for (int i = 1; i < bLineArray.size(); i++)
 			getSingleMovie(bLineArray.get(i));
 	}
 
@@ -57,23 +58,24 @@ public class TheatreManagement {
 	public static void createShowTimes(String[] singleMovie) {
 		String singleMovieTitleSplitArray[] = new String[200];
 		String formattedTitle = " ";
-		int movieDuration = 0;
+		//int movieDuration = 0;
 		for (int i = 0; i < singleMovie.length; i++) {
 			singleMovieTitleSplitArray = singleMovie[i].split(","); 
 			formattedTitle = singleMovieTitleSplitArray[0] + " - Rated " + singleMovieTitleSplitArray[2] + ", " + singleMovieTitleSplitArray[3];
-			movieDuration = convertIntoMinutes(singleMovieTitleSplitArray[3]);
+			//movieDuration = convertIntoMinutes(singleMovieTitleSplitArray[3]);
 			System.out.println(formattedTitle);
+			System.out.println("Schedule goes here");
 			if(!isWeekend(dayOfWeek)){
-					
+				System.out.println("It's not the weekend\n");
 				}
 			else{
-				
+				System.out.println("IT'S THE WEEKEND\n");
 			}
 		}
 	}
 	
-	public static String convertIntoDate(String theatreHours)throws ParseException{
-		Date day = sdf.parse(theatreHours);
+	public static String dateString(String theatreHours)throws ParseException{
+		day = sdf.parse(theatreHours);
 		String workDayHours = sdf.format(day);
 		return workDayHours;
 	}
@@ -88,9 +90,13 @@ public class TheatreManagement {
 
 	public static boolean isWeekend(int day){
 		if (day == 1 || day == 6 || day == 7)
-			weekend = true;
+			return true;
 		else
-			weekend = false;
-		return weekend;
+			return false;
+	}
+	
+	public static String printCurrentDate(){
+		return strDays[now.get(Calendar.DAY_OF_WEEK) - 1] + " " + (now.get(Calendar.MONTH) + 1) + "/"
+				+ now.get(Calendar.DATE) + "/" + now.get(Calendar.YEAR) + " ";
 	}
 }
